53dcbdd9fcdb396fcd96d09df5ca024fcbe16ba9
mcas0215 <kang33089343@gmail.com>
2025-06-07 17:41:31 +0900

    refactor UIStatus
    refactor UIStatus

--- Assets/Scripts/UIStatus.cs
+++ Assets/Scripts/UIStatus.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIStatus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text totalText;
+    public TMP_Text passionText;
+    public TMP_Text efficiencyText;
+    public TMP_Text healthText;
+    public TMP_Text socialText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
