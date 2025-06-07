9fbe5de9ed5d0ecaa43514b79b6edf13e0f35132
mcas0215 <kang33089343@gmail.com>
2025-06-07 16:52:50 +0900

    Refactor ItemData.cs
    Refactor ItemData.cs

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -2,8 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum Itemtype
+{
+    Passion,
+    Efficiency,
+    Health,
+    Social
+}
+
+
 public class ItemData : ScriptableObject
 {
-    
-    
+    public string displayName;
+    public string description;
+    public Itemtype type;
+    public Sprite Icon;
 }
